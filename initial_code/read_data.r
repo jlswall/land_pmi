75c441dedd40d72205c1e3366a72c64629d9277e
Jenise Swall <jenise.swall@gmail.com>
2017-06-29 19:47:08 -0400

    Began separating out the data into separate data frames.

--- initial_code/read_data.r
+++ initial_code/read_data.r
@@ -8,6 +8,51 @@ duplicated(colnames(phylumAllDF))
 min(which(duplicated(colnames(phylumAllDF))))
 
 
+## The first column contains "2"s for all the rows, except the very
+## last row (row #36).
+unique(phylumAllDF[1:35,1])
+phylumAllDF[36, 1]
+## The last row appears to be at the kingdom level, while the
+## preceding rows were at the phylum level.
+
+
+## ##################################################
+## Extract raw data for each pig in wide format.
+
+## For now, I'm going to completely ignore columns 2 ("rankID") and 4
+## ("daughterlevels").  I'm also going to ignore columsn beyond column
+## "DJ" (column 114).  These appear to be summary columns.
+## The columns I want to focus on are:
+## column 3: "taxon"
+## column 5: "total" - to check that the other numbers are correct.
+## columns 6-114
+##    columns whose names start with "A#" - These are the observed
+## counts for the individual pigs at the various times.
+##    columns whose names start with "T#" - These are the averages
+## of the observed counts over all pigs at the various times.
+
+## We're ignoring columns past 114.
+rawDF <- phylumAllDF[,1:114]
+
+## Identify column names starting with "A".
+namesA <- colnames(rawDF)[substring(first=1, last=1, colnames(rawDF))=="A"]
+wideIndivDF <- rawDF[,namesA]
+rm(namesA)
+
+## Identify column names starting with "T".  These are the averages of
+## the individual pigs at each time point.
+namesT <- colnames(rawDF)[substring(first=1, last=1, colnames(rawDF))=="T"]
+wideAvgsDF <- rawDF[,namesT]
+rm(namesT)
+## ##################################################
+
+
+library("tidyr")
+help("separate"
+## http://www.milanor.net/blog/reshape-data-r-tidyr-vs-reshape2/
+
+## ##################################################
+
 ## Colums DN (phylum names, column 118) and DO-ED (columns 119-134)
 ## seem to be averages from the earlier columns.
 phylumAllDF[1:5, 118:134]
