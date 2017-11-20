de31f314ac0d9fa4b6e90f30b46f7bdfabfa0995
Jenise Swall <jswall@vcu.edu>
2017-11-20 14:49:31 -0500

    Changed to allow us to switch easily between phyla, orders, and families.  Also, other minor chagnes.

--- initial_code/try_random_forest.r
+++ initial_code/try_random_forest.r
@@ -2,10 +2,13 @@ library("tidyverse")
 library("randomForest")
 
 
-## Read in cleaned-up phyla taxa.
-phylumT <- read_csv("phyla_massaged.csv")
-## Read in cleaned-up families taxa.
-familyT <- read_csv("families_massaged.csv")
+## ##################################################
+## Are we dealing with phlya, orders, or families?
+taxalevel <- "orders"
+
+## Read in cleaned-up phyla, orders, or families taxa.
+taxaT <- read_csv(paste0(taxalevel, "_massaged.csv"))
+## ##################################################
 
 
 
@@ -13,30 +16,27 @@ familyT <- read_csv("families_massaged.csv")
 ## Try random forests.
 
 ## Move back to wide format.
-widePercT <- familyT %>%
+wideT <- taxaT %>%
   ungroup() %>%
   select(degdays, subj, taxa, fracBySubjDay) %>%
   spread(taxa, fracBySubjDay)
-## One group ("rare or uncl.") has spaces in it, which can cause
-## problems when the tibble is converted to a data.frame.
-## colnames(widePercT) <- gsub(colnames(widePercT), pattern=" ", replacement="_")
     
 
 ## Pick subset of the data to train on.
-trainingIndices <- sort(sample(1:nrow(widePercT), size=60, replace=F))
-rf <- randomForest(degdays ~ . -subj -Rare, data=widePercT[trainingIndices,], importance=T)
+trainingIndices <- sort(sample(1:nrow(wideT), size=60, replace=F))
+rf <- randomForest(degdays ~ . -subj -Rare, data=wideT[trainingIndices,], importance=T)
 imp.rf <- importance(rf)
 varImpPlot(rf)
 ## For phylum taxa: Firmicutes strongest.  Bacteroidetes not strong.
 
 ## Now try to predict for those observations not in the training set.
-yhatTest <- predict(rf, newdata=widePercT[-trainingIndices,])
-mean((yhatTest - widePercT[-trainingIndices,"degdays"])^2)
+yhatTest <- predict(rf, newdata=wideT[-trainingIndices,])
+mean((yhatTest - wideT[-trainingIndices,"degdays"])^2)
 
 par(mfrow=c(2,2))
 impvar <- rownames(imp.rf)[order(imp.rf[,1], decreasing=T)]
 for (i in seq_along(impvar))
-  partialPlot(rf, pred.data=as.data.frame(widePercT[trainingIndices,]), x.var=impvar[i], main=paste("Partial Dependence on", impvar[i]))
+  partialPlot(rf, pred.data=as.data.frame(wideT[trainingIndices,]), x.var=impvar[i], main=paste("Partial Dependence on", impvar[i]))
 rm(i)
 ## ##################################################
 
@@ -47,24 +47,24 @@ rm(i)
 
 
 ## Try gamsel package.
-trainX <- as.data.frame(widePercT %>%
+trainX <- as.data.frame(wideT %>%
                         select(-one_of("degdays", "subj", "Rare_or_uncl."))
                         )[trainingIndices,]
-trainY <- as.matrix(as.data.frame(widePercT[trainingIndices, "degdays"]))
+trainY <- as.matrix(as.data.frame(wideT[trainingIndices, "degdays"]))
 gamsel.out <- gamsel(x=trainX, y=trainY, dfs=rep(10, ncol(trainX)))
 summary(gamsel.out)
 gamsel.cv <- cv.gamsel(x=trainX, y=trainY, dfs=rep(10, ncol(trainX)))
 
 
-gam.fit <- gam(degdays ~ s(Firmicutes) + s(Actinobacteria), data=widePercT[trainingIndices,])
-big.fit <- gam(degdays ~ s(Firmicutes) + s(Actinobacteria) + s(Proteobacteria), data=widePercT[trainingIndices,])
+gam.fit <- gam(degdays ~ s(Firmicutes) + s(Actinobacteria), data=wideT[trainingIndices,])
+big.fit <- gam(degdays ~ s(Firmicutes) + s(Actinobacteria) + s(Proteobacteria), data=wideT[trainingIndices,])
 summary(gam.fit)
-yhatTest <- predict(gam.fit, newdata=widePercT[-trainingIndices,])
+yhatTest <- predict(gam.fit, newdata=wideT[-trainingIndices,])
 
 ## Show fitted values vs. real values.
-plot(unlist(widePercT[trainingIndices, "degdays"]), gam.fit$fitted)
-cor(unlist(widePercT[trainingIndices, "degdays"]), gam.fit$fitted)
+plot(unlist(wideT[trainingIndices, "degdays"]), gam.fit$fitted)
+cor(unlist(wideT[trainingIndices, "degdays"]), gam.fit$fitted)
 ## Show predicted values vs. real values.
-plot(unlist(widePercT[-trainingIndices, "degdays"]), unlist(yhatTest))
-cor(unlist(widePercT[-trainingIndices, "degdays"]), unlist(yhatTest)
+plot(unlist(wideT[-trainingIndices, "degdays"]), unlist(yhatTest))
+cor(unlist(wideT[-trainingIndices, "degdays"]), unlist(yhatTest)
 ## ##################################################
