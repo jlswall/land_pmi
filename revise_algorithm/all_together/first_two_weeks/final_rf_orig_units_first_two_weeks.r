a10e3ada71dd0e9ea43e4d75c58e886ad60d8371
Jenise Swall <jswall@vcu.edu>
2018-02-09 12:08:30 -0500

    Added separate directory for analyses done with first 2 weeks of data.

---
+++ revise_algorithm/all_together/first_two_weeks/final_rf_orig_units_first_two_weeks.r
@@ -0,0 +1,100 @@
+library("tidyverse")
+library("randomForest")
+library("figdim")
+
+
+## In the first 15 days (448 degree days), observations were made
+## approx. every other day.  Then, there's a big gap between 15 days
+## and the next observation 26 days out.  Let's try the analysis with
+## just these observations.
+
+
+## ##################################################
+## Read in the data.
+
+## Read in the combined data in wide format.
+tmpT <- read_csv("combined_taxa.csv")
+
+## Remove subj and days variable, since they won't be used in the
+## random forest.
+earlyT <- tmpT %>%
+  filter(days <= 15) %>%
+  select(-subj, -days)
+
+## Just for reference later, keep the days and degree days, so we can
+## look at the time correspondence.
+timeT <- tmpT %>% distinct(days, degdays)
+
+rm(tmpT)
+## ##################################################
+
+
+
+## ##################################################
+## From earlier experiments, we figured out these parameters work best
+## for the random forest model.
+
+## Number of bootstrap samples.
+numBtSamps <- 5000
+
+## Early runs indicated that the number of splits is around 29-31.
+numVarSplit <- 30
+## ##################################################
+
+
+
+## ##################################################
+## Run the cross-validation for this model, so that we can see what
+## the CV MSE looks like.
+
+## Number of times to do cross-validation.
+numCVs <- 100
+## ## How many observations to reserve for testing each time.
+numLeaveOut <- round(0.10 * nrow(earlyT))
+
+## For matrix to hold cross-validation results.
+cvMSE <- rep(NA, numCVs)
+cvErrFrac <- rep(NA, numCVs)
+
+set.seed(431974)
+
+## Do cross-validation.
+for (i in 1:numCVs){
+  
+  ## Determine training and cross-validation set.
+  whichLeaveOut <- sample(1:nrow(earlyT), size=numLeaveOut, replace=F)    
+  subT <- earlyT[-whichLeaveOut,]
+  cvsetT <- earlyT[whichLeaveOut,]
+  
+  ## Calculate SSTotal for the cross-validation set.
+  SSTot <- sum( (cvsetT$degdays-mean(cvsetT$degdays))^2 )
+
+  rf <- randomForest(degdays ~ . , data=subT, mtry=numVarSplit,
+                     ntree=numBtSamps, importance=T)
+  fitTest <- predict(rf, newdata=cvsetT)
+    
+  fitResid <- fitTest - cvsetT$degdays
+  cvMSE[i] <- mean(fitResid^2)
+  cvErrFrac[i] <- sum(fitResid^2)/SSTot
+}
+rm(whichLeaveOut, subT, cvsetT, SSTot, rf, fitTest, fitResid, numLeaveOut, i)
+
+
+write_csv(data.frame(cvMSE, cvErrFrac), path="final_rf_orig_units_cvstats_first_two_weeks.csv")
+rm(cvMSE, cvErrFrac)
+## ##################################################
+
+
+
+## ##################################################
+## Fit the final random forest with all the data (no cross-validation).
+
+set.seed(743931)
+
+## Fit the random forest model on all the data (no cross-validation).
+rf <- randomForest(degdays ~ . , data=earlyT, mtry=numVarSplit,
+                   ntree=numBtSamps, importance=T)
+
+init.fig.dimen(file=paste0("orig_units_first_two_weeks_combined_imp_plot.pdf"), width=8, height=6)
+varImpPlot(rf, main="Importance of combined taxa (first 2 weeks)")
+dev.off()
