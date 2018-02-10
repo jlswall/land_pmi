99896d5aa96dd58ea0ee3531480eb88ec7ad824a
Jenise Swall <jenise.swall@gmail.com>
2018-02-10 15:57:08 -0500

    Finished with family taxa, first 2 weeks, orig. and sqrt. units.

---
+++ revise_algorithm/only_families/first_two_weeks/final_rf_orig_units_first_two_weeks.r
@@ -0,0 +1,109 @@
+library("tidyverse")
+library("randomForest")
+library("figdim")
+
+## In the first 15 days (448 degree days), observations were made
+## approx. every other day.  Then, there's a big gap between 15 days
+## and the next observation 26 days out.  Let's try the analysis with
+## just these observations.
+
+
+## ##################################################
+## Are we dealing with phlya, orders, or families?
+taxalevel <- "families"
+
+## Read in cleaned-up phyla, orders, or families taxa.
+taxaT <- read_csv(paste0("../../", taxalevel, "_massaged.csv"), col_types="iiccnn")
+## ##################################################
+
+
+
+## ##################################################
+## Put the data in wide format and restrict to the first 15 days.
+
+## Move back to wide format.
+earlyT <- taxaT %>%
+  filter((days <= 15) & (taxa!="Rare")) %>%
+  select(degdays, subj, taxa, fracBySubjDay) %>%
+  spread(taxa, fracBySubjDay) %>%
+  select(-subj)
+
+## Just for reference later, keep the days and degree days, so we can
+## look at the time correspondence.
+timeT <- taxaT %>% distinct(days, degdays)
+
+rm(taxaT)
+## ##################################################
+
+
+
+## ##################################################
+## From earlier experiments, we figured out these parameters work best
+## for the random forest model.
+
+## Number of bootstrap samples.
+numBtSamps <- 5000
+
+## Early runs indicated that the number of variables to consider at
+## each split is about 15 for response variable in the original units.
+numVarSplit <- 15
+## ##################################################
+
+
+## ##################################################
+## Run the cross-validation for this model, so that we can see what
+## the CV MSE looks like.
+
+## Number of times to do cross-validation.
+numCVs <- 100
+## ## How many observations to reserve for testing each time.
+numLeaveOut <- round(0.10 * nrow(earlyT))
+
+
+## For matrix to hold cross-validation results.
+cvMSE <- rep(NA, numCVs)
+cvErrFrac <- rep(NA, numCVs)
+
+set.seed(9046179)
+
+
+## Do cross-validation.
+for (i in 1:numCVs){
+  
+  ## Determine training and cross-validation set.
+  whichLeaveOut <- sample(1:nrow(earlyT), size=numLeaveOut, replace=F)    
+  subT <- earlyT[-whichLeaveOut,]
+  cvsetT <- earlyT[whichLeaveOut,]
+  
+  ## Calculate SSTotal for the cross-validation set.
+  SSTot <- sum( (cvsetT$degdays-mean(cvsetT$degdays))^2 )
+
+  rf <- randomForest(degdays ~ . , data=subT, mtry=numVarSplit,
+                     ntree=numBtSamps, importance=T)
+  fitTest <- predict(rf, newdata=cvsetT)
+    
+  fitResid <- fitTest - cvsetT$degdays
+  cvMSE[i] <- mean(fitResid^2)
+  cvErrFrac[i] <- sum(fitResid^2)/SSTot
+}
+rm(whichLeaveOut, subT, cvsetT, SSTot, rf, fitTest, fitResid, numLeaveOut, i)
+
+write_csv(data.frame(cvMSE, cvErrFrac), path="final_rf_orig_units_cvstats_first_two_weeks.csv")
+rm(cvMSE, cvErrFrac)
+## ##################################################
+
+
+
+## ##################################################
+## Fit the final random forest with all the data (no cross-validation).
+
+set.seed(7186931)
+
+## Fit the random forest model on all the data (no cross-validation).
+rf <- randomForest(degdays ~ . , data=earlyT, mtry=numVarSplit,
+                   ntree=numBtSamps, importance=T)
+
+init.fig.dimen(file=paste0("orig_units_first_two_weeks_families_imp_plot.pdf"), width=8, height=6)
+varImpPlot(rf, main="Importance of family taxa (orig. units, first 2 weeks)")
+dev.off()
+## ##################################################
