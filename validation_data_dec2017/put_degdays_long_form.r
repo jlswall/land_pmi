0a84f4f84177854435210a66cc5cdf9e8196466d
Jenise Swall <jswall@vcu.edu>
2018-04-10 12:10:59 -0400

    My original version of this file accidentally left out the days column.  That's been fixed.

--- validation_data_dec2017/put_degdays_long_form.r
+++ validation_data_dec2017/put_degdays_long_form.r
@@ -16,22 +16,27 @@ rawT <- rawT[,-7]
 ## Separate out and rename the first 2 columns for pig 1.  Add a column with the
 ## subject number.
 pig1T <- rawT[,1:2]
-colnames(pig1T) <- c("subj", "degdays")
+colnames(pig1T) <- c("days", "degdays")
 pig1T$subj <- "P1"
 ## Separate out and rename columns 3-4 for pig 2.  Add a column with the
 ## subject number.
 pig2T <- rawT[,3:4]
-colnames(pig2T) <- c("subj", "degdays")
+colnames(pig2T) <- c("days", "degdays")
 pig2T$subj <- "P2"
 ## Separate out and rename columns 5-6 for pig 3.  Add a column with the
 ## subject number.
 pig3T <- rawT[,5:6]
-colnames(pig3T) <- c("subj", "degdays")
+colnames(pig3T) <- c("days", "degdays")
 pig3T$subj <- "P3"
 
 
 ## Now, combined these into a long form dataset.
 longT <- rbind(pig1T, pig2T, pig3T)
 rm(pig1T, pig2T, pig3T)
+
+
+## Remove the "T" from the "T0", "T1", etc. in the days column.
+longT$days <- substring(longT$days, first=2, last=2)
+
 
 write_csv(longT, path="degdays_by_subj_day.csv")
