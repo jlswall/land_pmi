12bcb017b953737a1fdfe2465ee7490baf82ef39
Jenise Swall <jenise.swall@gmail.com>
2018-05-11 13:48:10 -0400

    Added code for the orders of the eukaryote data, all time steps.

---
+++ eukaryote_data/only_orders/all_time_steps/final_rf_orig_units_all_data.r
@@ -0,0 +1,116 @@
+library("tidyverse")
+library("randomForest")
+library("figdim")
+
+
+## ##################################################
+## Are we dealing with phlya, orders, or families?
+taxalevel <- "orders"
+
+## Read in cleaned-up phyla, orders, or families taxa.
+taxaT <- read_csv(paste0("../../", taxalevel, "_massaged.csv"))
+## ##################################################
+
+
+
+## ##################################################
+## Put the data in wide format; remove days, subj, and rare taxa.
+
+## Move back to wide format.
+allT <- taxaT %>%
+  filter(taxon!="Rare") %>%
+  select(degdays, subj, taxon, fracBySubjDay) %>%
+  spread(taxon, fracBySubjDay) %>%
+  select(-subj)
+
+## Just for reference later, keep the days and degree days, so we can
+## look at the time correspondence.
+timeT <- taxaT %>% distinct(days, degdays)
+
+rm(taxaT)
+## ##################################################
+
+
+
+## ##################################################
+## From earlier experiments, we figured out these parameters work best
+## for the random forest model.
+
+## Number of bootstrap samples.
+numBtSamps <- 4000
+
+## Repeated cross-validation runs, leaving out 20% of the observations
+## at a time, indicated that the number of variables to consider at
+## each split is about 17 for the response variable in the original
+## units (although 16 and 18 are close).
+numVarSplit <- 17
+## ##################################################
+
+
+
+## ##################################################
+## Run the cross-validation for this model, so that we can see what
+## the CV MSE looks like.
+
+## Number of times to do cross-validation.
+numCVs <- 1000
+## How many observations to reserve for testing each time.
+numLeaveOut <- round(0.20 * nrow(allT))
+
+
+## For matrix to hold cross-validation results.
+cvMSE <- rep(NA, numCVs)
+cvErrFrac <- rep(NA, numCVs)
+
+set.seed(3483029)
+
+
+## Do cross-validation.
+for (i in 1:numCVs){
+  
+  ## Determine training and cross-validation set.
+  whichLeaveOut <- sample(1:nrow(allT), size=numLeaveOut, replace=F)    
+  subT <- allT[-whichLeaveOut,]
+  cvsetT <- allT[whichLeaveOut,]
+  
+  ## Calculate SSTotal for the cross-validation set.
+  SSTot <- sum( (cvsetT$degdays-mean(cvsetT$degdays))^2 )
+
+  rf <- randomForest(degdays ~ . , data=subT, mtry=numVarSplit,
+                     ntree=numBtSamps, importance=T)
+  fitTest <- predict(rf, newdata=cvsetT)
+    
+  fitResid <- fitTest - cvsetT$degdays
+  cvMSE[i] <- mean(fitResid^2)
+  cvErrFrac[i] <- sum(fitResid^2)/SSTot
+}
+rm(whichLeaveOut, subT, cvsetT, SSTot, rf, fitTest, fitResid, numLeaveOut, i)
+
+write_csv(data.frame(cvMSE, cvErrFrac), path="final_rf_orig_units_cvstats_all_data.csv")
+rm(cvMSE, cvErrFrac)
+## ##################################################
+
+
+
+## ##################################################
+## Fit the final random forest with all the data (no cross-validation).
+
+set.seed(822933)
+
+## Fit the random forest model on all the data (no cross-validation).
+rf <- randomForest(degdays ~ . , data=allT, mtry=numVarSplit,
+                   ntree=numBtSamps, importance=T)
+
+init.fig.dimen(file=paste0("orig_units_all_data_orders_imp_plot.pdf"), width=8, height=6)
+varImpPlot(rf, main="Importance of order taxa (orig. units, all time steps)")
+dev.off()
+
+
+## Find residuals:
+resids <- rf$predicted - allT$degdays
+## Print out RMSE:
+sqrt( mean( resids^2 ) )
+## Estimate of explained variance, which R documentation calls "pseudo
+## R-squared"
+1 - ( sum(resids^2)/sum( (allT$degdays - mean(allT$degdays))^2 ) )
+## ##################################################
