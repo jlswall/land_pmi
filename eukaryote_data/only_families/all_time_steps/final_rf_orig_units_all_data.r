b2eca31b73777755f87e82927437e36cbb73f478
Jenise Swall <jenise.swall@gmail.com>
2018-05-12 14:27:11 -0400

    Finished running models for family data, using all time steps.

---
+++ eukaryote_data/only_families/all_time_steps/final_rf_orig_units_all_data.r
@@ -0,0 +1,116 @@
+library("tidyverse")
+library("randomForest")
+library("figdim")
+
+
+## ##################################################
+## Are we dealing with phlya, orders, or families?
+taxalevel <- "families"
+
+## Read in cleaned-up phyla, orders, or families taxa.
+taxaT <- read_csv(paste0("../../", taxalevel, "_massaged.csv"))
+## ##################################################
+
+
+
+## ##################################################
+## Put the data in wide format; remove days, subj, and rare taxa.
+
+## Move back to wide format.
+allT <- taxaT %>%
+  filter(taxon!="Rare") %>%
+  select(degdays, subj, taxon, fracBySubjDay) %>%
+  spread(taxon, fracBySubjDay) %>%
+  select(-subj)
+
+## Just for reference later, keep the days and degree days, so we can
+## look at the time correspondence.
+timeT <- taxaT %>% distinct(days, degdays)
+
+rm(taxaT)
+## ##################################################
+
+
+
+## ##################################################
+## From earlier experiments, we figured out these parameters work best
+## for the random forest model.
+
+## Number of bootstrap samples.
+numBtSamps <- 3000
+
+## Repeated cross-validation runs, leaving out 20% of the observations
+## at a time, indicated that the number of variables to consider at
+## each split is about 18 for the response variable in the original
+## units (although 17 and 19 are close).
+numVarSplit <- 18
+## ##################################################
+
+
+
+## ##################################################
+## Run the cross-validation for this model, so that we can see what
+## the CV MSE looks like.
+
+## Number of times to do cross-validation.
+numCVs <- 1000
+## How many observations to reserve for testing each time.
+numLeaveOut <- round(0.20 * nrow(allT))
+
+
+## For matrix to hold cross-validation results.
+cvMSE <- rep(NA, numCVs)
+cvErrFrac <- rep(NA, numCVs)
+
+set.seed(5485029)
+
+
+## Do cross-validation.
+for (i in 1:numCVs){
+  
+  ## Determine training and cross-validation set.
+  whichLeaveOut <- sample(1:nrow(allT), size=numLeaveOut, replace=F)    
+  subT <- allT[-whichLeaveOut,]
+  cvsetT <- allT[whichLeaveOut,]
+  
+  ## Calculate SSTotal for the cross-validation set.
+  SSTot <- sum( (cvsetT$degdays-mean(cvsetT$degdays))^2 )
+
+  rf <- randomForest(degdays ~ . , data=subT, mtry=numVarSplit,
+                     ntree=numBtSamps, importance=T)
+  fitTest <- predict(rf, newdata=cvsetT)
+    
+  fitResid <- fitTest - cvsetT$degdays
+  cvMSE[i] <- mean(fitResid^2)
+  cvErrFrac[i] <- sum(fitResid^2)/SSTot
+}
+rm(whichLeaveOut, subT, cvsetT, SSTot, rf, fitTest, fitResid, numLeaveOut, i)
+
+write_csv(data.frame(cvMSE, cvErrFrac), path="final_rf_orig_units_cvstats_all_data.csv")
+rm(cvMSE, cvErrFrac)
+## ##################################################
+
+
+
+## ##################################################
+## Fit the final random forest with all the data (no cross-validation).
+
+set.seed(8229331)
+
+## Fit the random forest model on all the data (no cross-validation).
+rf <- randomForest(degdays ~ . , data=allT, mtry=numVarSplit,
+                   ntree=numBtSamps, importance=T)
+
+init.fig.dimen(file=paste0("orig_units_all_data_families_imp_plot.pdf"), width=8, height=6)
+varImpPlot(rf, main="Importance of family taxa (orig. units, all time steps)")
+dev.off()
+
+
+## Find residuals:
+resids <- rf$predicted - allT$degdays
+## Print out RMSE:
+sqrt( mean( resids^2 ) )
+## Estimate of explained variance, which R documentation calls "pseudo
+## R-squared"
+1 - ( sum(resids^2)/sum( (allT$degdays - mean(allT$degdays))^2 ) )
+## ##################################################
---
+++ eukaryote_data/only_families/all_time_steps/repeated_cv_leave_out_20perc.r
@@ -0,0 +1,195 @@
+library("tidyverse")
+library("randomForest")
+library("figdim")
+library("parallel")
+
+
+## ##################################################
+## Are we dealing with phlya, orders, or families?
+taxalevel <- "families"
+
+## Read in cleaned-up phyla, orders, or families taxa.
+taxaT <- read_csv(paste0("../../", taxalevel, "_massaged.csv"))
+## ##################################################
+
+
+
+## ##################################################
+## Put the data in wide format; remove days, subj, and rare taxa.
+
+## Move back to wide format.
+allT <- taxaT %>%
+  filter(taxon!="Rare") %>%
+  select(degdays, subj, taxon, fracBySubjDay) %>%
+  spread(taxon, fracBySubjDay) %>%
+  select(-subj)
+
+## Just for reference later, keep the days and degree days, so we can
+## look at the time correspondence.
+timeT <- taxaT %>% distinct(days, degdays)
+
+rm(taxaT)
+## ##################################################
+
+
+
+## ##################################################
+## Try random forests for regression using "days" as the response
+## variable.
+
+## #########
+## How many predictors?  (All columns except response: "degdays").
+numPredictors <- ncol(allT) - 1
+
+## Try different numbers of bootstrap samples.
+numBtSampsVec <- c(480, 1200, 2400, 3000, 3600)
+
+## Try different values for mtry (which represents how many variables
+## can be chosen from at each split of the tree).
+numVarSplitVec <- seq(12, 21, by=1)
+
+## Form matrix with all combinations of these.
+combos <- expand.grid(numBtSamps=numBtSampsVec, numVarSplit=numVarSplitVec)
+## #########
+
+
+## ###########################
+## Do cross-validation over and over, leaving out a different 20% of
+## the 93 observations each time.
+
+set.seed(7599944)
+
+## Number of times to do cross-validation.
+numCVs <- 1000
+## How many observations to reserve for testing each time.
+numLeaveOut <- round(0.20 * nrow(allT))
+
+
+## For matrix to hold cross-validation results.
+cvMSE <- matrix(NA, nrow(combos), ncol=numCVs)
+cvErrFrac <- matrix(NA, nrow(combos), ncol=numCVs)
+origUnitsqrtcvMSE <- matrix(NA, nrow(combos), ncol=numCVs)
+origUnitsqrtcvErrFrac <- matrix(NA, nrow(combos), ncol=numCVs)
+sqrtcvMSE <- matrix(NA, nrow(combos), ncol=numCVs)
+sqrtcvErrFrac <- matrix(NA, nrow(combos), ncol=numCVs)
+
+
+
+## #########################################
+## Set up function for fitting random forest model using original
+## units.
+origUnitsF <- function(x, jCombo){
+  rf <- randomForest(degdays ~ . , data=x$trainT, mtry=combos[jCombo, "numVarSplit"], ntree=combos[jCombo, "numBtSamps"], importance=T)
+  return(predict(rf, newdata=x$validT))
+}
+
+## Set up function for fitting random forest model using square root
+## units.
+sqrtUnitsF <- function(x, jCombo){
+  sqrtrf <- randomForest(sqrt(degdays) ~ . , data=x$trainT, mtry=combos[jCombo, "numVarSplit"], ntree=combos[jCombo, "numBtSamps"], importance=T)
+  return(predict(sqrtrf, newdata=x$validT))
+}
+## #########################################
+
+
+## #########################################
+## Get set up for cross-validation.
+crossvalidL <- vector("list", numCVs)
+for (i in 1:numCVs){
+  lvOut <- sample(1:nrow(allT), size=numLeaveOut, replace=F)
+  trainT <- allT[-lvOut,]
+  validT <- allT[lvOut,]
+  crossvalidL[[i]] <- list(trainT=trainT, validT=validT)
+}
+rm(i, lvOut, trainT, validT)
+
+
+## Try using lapply to fit the random forests.
+origFitL <- vector("list", nrow(combos))
+for (j in 1:nrow(combos)){
+  origFitL[[j]] <- mclapply(crossvalidL, mc.cores=6, origUnitsF, jCombo=j)
+  if (j %% 2 == 0)
+    print(paste0("In orig units, finished combo number ", j))
+}    
+rm(j)
+
+sqrtFitL <- vector("list", nrow(combos))
+for (j in 1:nrow(combos)){
+  sqrtFitL[[j]] <- mclapply(crossvalidL, mc.cores=6, sqrtUnitsF, jCombo=j)
+  if (j %% 2 == 0)
+    print(paste0("In sqrt units, finished combo number ", j))
+}
+rm(j)
+## #########################################
+
+
+## #########################################
+## Now, calculate the various summary statistics for each cross-validation.
+
+for (i in 1:numCVs){
+
+  ## Get the validation set for this run from the list.
+  validT <- crossvalidL[[i]][["validT"]]
+
+  ## Calculate SSTotal for the cross-validation set.
+  SSTot <- sum( (validT$degdays-mean(validT$degdays))^2 )
+  
+  for (j in 1:nrow(combos)){
+     
+    ## Calculate the MSE and error fraction of the SS Total for the
+    ## validation data in the original units.
+    resid <- origFitL[[j]][[i]] - validT$degdays
+    cvMSE[j,i] <- mean(resid^2)
+    cvErrFrac[j,i] <- sum(resid^2)/SSTot
+    rm(resid)
+  
+    ## Calculate the MSE and error fraction of the SS Total for the
+    ## validation data in the original units.
+    sqrtUnitResid <- sqrtFitL[[j]][[i]] - sqrt(validT$degdays)
+    origUnitResid <- sqrtFitL[[j]][[i]]^2 - validT$degdays
+    sqrtcvMSE[j,i] <- mean(sqrtUnitResid^2)
+    sqrtcvErrFrac[j,i] <- sum(sqrtUnitResid^2)/sum( ( sqrt(validT$degdays) - mean(sqrt(validT$degdays)) )^2 )
+    origUnitsqrtcvMSE[j,i] <- mean(origUnitResid^2)
+    origUnitsqrtcvErrFrac[j,i] <- sum(origUnitResid^2)/SSTot
+    rm(sqrtUnitResid, origUnitResid)
+  }
+}
+rm(i, j, validT, SSTot)
+## #########################################
+
+
+
+## #########################################
+## Calculate summary statistics over all the cross-validations.
+
+combos$avgcvMSE <- apply(cvMSE, 1, mean)
+combos$avgcvErrFrac <- apply(cvErrFrac, 1, mean)
+
+combos$avgsqrtcvMSE <- apply(sqrtcvMSE, 1, mean)
+combos$avgsqrtcvErrFrac <- apply(sqrtcvErrFrac, 1, mean)
+combos$avgorigUnitsqrtcvMSE <- apply(origUnitsqrtcvMSE, 1, mean)
+combos$avgorigUnitsqrtcvErrFrac <- apply(origUnitsqrtcvErrFrac, 1, mean)
+
+write_csv(combos, path="repeated_cv_leave_out_20perc.csv")
+## #########################################
+
+
+
+## #########################################
+## Make plots showing these statistics for various combinations of
+## numbers of boot strap samples and number of variables to consider
+## at each split.
+
+ggplot(data=combos, aes(x=numBtSamps, y=avgcvMSE, color=as.factor(numVarSplit))) + geom_line()
+## X11()
+ggplot(data=combos, aes(x=numBtSamps, y=avgsqrtcvMSE, color=as.factor(numVarSplit))) + geom_line()
+## X11()
+ggplot(data=combos, aes(x=numBtSamps, y=avgorigUnitsqrtcvMSE, color=as.factor(numVarSplit))) + geom_line()
+
+
+ggplot(data=combos, aes(x=numBtSamps, y=avgcvErrFrac, color=as.factor(numVarSplit))) + geom_line()
+## X11()
+ggplot(data=combos, aes(x=numBtSamps, y=avgsqrtcvErrFrac, color=as.factor(numVarSplit))) + geom_line()
+## X11()
+ggplot(data=combos, aes(x=numBtSamps, y=avgorigUnitsqrtcvErrFrac, color=as.factor(numVarSplit))) + geom_line()
+## ####################
