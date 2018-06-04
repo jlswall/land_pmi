a8973c5d76233c3e4a5f22625f394390231c5d4c
Jenise Swall <jenise.swall@gmail.com>
2018-06-04 16:07:40 -0400

    Deleted the output from a previous batch job.  Updated the R code by cleaning it up a little (deleting some old comments), and by changing which numbers of variables to consider at each decision tree split.

--- revise_algorithm/only_orders/all_time_steps/parallel_leave_out_20perc.r
+++ revise_algorithm/only_orders/all_time_steps/parallel_leave_out_20perc.r
@@ -43,11 +43,10 @@ numPredictors <- ncol(allT) - 1
 
 ## Try different numbers of bootstrap samples.
 numBtSampsVec <- c(1400, 2400, 3000, 3600, 4000)
-## numBtSampsVec <- seq(4000, 5000, by=1000)
 
 ## Try different values for mtry (which represents how many variables
 ## can be chosen from at each split of the tree).
-numVarSplitVec <- seq(9, 14, by=1)
+numVarSplitVec <- seq(3, 21, by=3)
 
 ## Form matrix with all combinations of these.
 combos <- expand.grid(numBtSamps=numBtSampsVec, numVarSplit=numVarSplitVec)
@@ -156,48 +155,6 @@ for (i in 1:numCVs){
 }
 rm(i, j, validT, SSTot)
 ## #########################################
-
-
-
-## ## Do cross-validation.
-## for (i in 1:numCVs){
-  
-##   ## Determine training and cross-validation set.
-##   whichLeaveOut <- sample(1:nrow(allT), size=numLeaveOut, replace=F)    
-##   subT <- allT[-whichLeaveOut,]
-##   cvsetT <- allT[whichLeaveOut,]
-  
-##   ## Calculate SSTotal for the cross-validation set.
-##   SSTot <- sum( (cvsetT$degdays-mean(cvsetT$degdays))^2 )
-
-##   for (j in 1:nrow(combos)){
-##     rf <- foreach(ntree=rep(treesPerCore, 4), .combine=combine, .packages='randomForest') %dopar% randomForest(degdays ~ . , data=subT, mtry=combos[j,"numVarSplit"], ntree=combos[j,"numBtSamps"], importance=T)
-##     fitTest <- predict(rf, newdata=cvsetT)
-    
-##     fitResid <- fitTest - cvsetT$degdays
-##     cvMSE[j,i] <- mean(fitResid^2)
-##     cvErrFrac[j,i] <- sum(fitResid^2)/SSTot
-##   }
-##   rm(rf, fitTest, fitResid)
-
-##   ## In sqrt units:
-##   for (j in 1:nrow(combos)){
-##     sqrtrf <- foreach(ntree=rep(treesPerCore, 4), .combine=combine, .packages='randomForest') %dopar% randomForest(sqrt(degdays) ~ . , data=subT, mtry=combos[j,"numVarSplit"], ntree=combos[j,"numBtSamps"], importance=T)
-##     sqrtfitTest <- predict(sqrtrf, newdata=cvsetT)
-##     sqrtfitResid <- sqrtfitTest - sqrt(cvsetT$degdays)
-##     origUnitResid <- sqrtfitTest^2 - cvsetT$degdays
-    
-##     sqrtcvMSE[j,i] <- mean(sqrtfitResid^2)
-##     sqrtcvErrFrac[j,i] <- sum(sqrtfitResid^2)/sum( ( sqrt(cvsetT$degdays) - mean(sqrt(cvsetT$degdays)) )^2 )
-##     origUnitsqrtcvMSE[j,i] <- mean(origUnitResid^2)
-##     origUnitsqrtcvErrFrac[j,i] <- sum(origUnitResid^2)/SSTot
-##   }
-##   rm(sqrtrf, sqrtfitTest, sqrtfitResid, origUnitResid)
-
-##   if (i %% 10 == 0)
-##     print(paste0("Finishing cross-validation number ", i))
-## }
-## rm(i, j, SSTot)
 
 
 
