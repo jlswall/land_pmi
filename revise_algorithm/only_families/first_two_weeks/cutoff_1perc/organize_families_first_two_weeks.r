6908d3d3a78f8805bbd9f979da32e029e9c7cf46
Jenise Swall <jenise.swall@gmail.com>
2018-06-07 11:29:12 -0400

    Getting ready to run new cross-validations for first 2 weeks of families data, using parallel computing and leaving 20% of the data out which each fitting.

---
+++ revise_algorithm/only_families/first_two_weeks/cutoff_1perc/organize_families_first_two_weeks.r
@@ -0,0 +1,73 @@
+library("tidyverse")
+
+
+## Check that all of these taxa have at least one day and one cadaver
+## during the first couple of weeks for which the frequency exceeds
+## the cutoff for at least one individual.  Otherwise, those taxa
+## should be considered "rare" for an analysis of just the first 2
+## weeks.
+freqCutoff <- 0.01
+
+
+
+## ##################################################
+## Are we dealing with phlya, orders, or families?
+taxalevel <- "families"
+
+## Read in cleaned-up phyla, orders, or families taxa.
+taxaT <- read_csv(paste0("../../../", taxalevel, "_massaged.csv"))
+
+## We're only looking at the first 15 days (approx. 2 weeks).
+earlyT <- taxaT %>% filter(days <= 15)
+
+rm(taxaT)
+## ##################################################
+
+
+
+## ##################################################
+## Any taxa which don't make up at least a certain percentage (given
+## by freqCutoff) on at least 1 day and 1 cadaver (during the first 2
+## weeks) will get absorbed into the "rare" category.
+
+## Get list of taxa for which the percentage never exceeds the
+## frequency cutoff, for any day or individual.
+freqTaxaT <- earlyT %>%
+  group_by(taxa) %>%
+  summarize(maxFracBySubjDay = max(fracBySubjDay)) %>%
+  filter(maxFracBySubjDay >= freqCutoff) %>%
+  arrange(desc(maxFracBySubjDay)) %>%
+  select(taxa)
+
+
+## Incorporate the taxa that occur less than the frequency cutoff into
+## the "rare" group.
+commontaxaT <- earlyT
+commontaxaT[!(commontaxaT$taxa %in% freqTaxaT$taxa), "taxa"] <- "Rare"
+commontaxaT <- commontaxaT %>%
+  group_by(days, degdays, subj, taxa) %>%
+  summarize(counts=sum(counts), fracBySubjDay=sum(fracBySubjDay)) %>%
+  ungroup()
+
+
+## Check that the fractions add up to 1, appropriately.
+unique(
+    commontaxaT %>%
+    group_by(days, subj) %>%
+    summarize(sumFracBySubjDay = sum(fracBySubjDay)) %>%
+    ungroup() %>%
+    select(sumFracBySubjDay)
+)
+
+
+## Remove the list of taxa names that satisfied the frequency cutoff.
+rm(freqTaxaT, earlyT)
+## ##################################################
+
+
+
+## ##################################################
+## Write out the data to use in analyses for the first 2 weeks.
+
+write.csv(commontaxaT, file="families_only_first_two_weeks.csv", row.names=FALSE)
+## ##################################################
