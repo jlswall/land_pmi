f8b7d9d4329af312da5ab3492afc49930c7e7cc8
Jenise Swall <jenise.swall@gmail.com>
2018-06-08 15:52:58 -0400

    Completed simulation for the orders data using the 1% rule for 2 individuals.  Finished the final random forest code for the original units, but I still need to finish the model for the sqrt units.

---
+++ eukaryote_data/only_orders/all_time_steps/hit_1perc_twice/final_rf_orig_units_all_data.r
@@ -0,0 +1,143 @@
+library("tidyverse")
+library("randomForest")
+library("figdim")
+library("parallel")
+
+
+## ##################################################
+## Are we dealing with phlya, orders, or families?
+taxalevel <- "orders"
+
+## Read in cleaned-up phyla, orders, or families taxa.
+taxaT <- read_csv(paste0(taxalevel, "_hit_cutoff_twice_all_time_steps.csv"))
+## ##################################################
+
+
+
+## ##################################################
+## Put the data in wide format; remove days, subj, and rare taxa.
+
+## Move back to wide format.
+wideT <- taxaT %>%
+  filter(taxon!="Rare") %>%
+  select(degdays, subj, taxon, fracBySubjDay) %>%
+  spread(taxon, fracBySubjDay) %>%
+  select(-subj)
+
+## Just for reference later, keep the days and degree days, so we can
+## look at the time correspondence.
+timeT <- taxaT %>% distinct(days, degdays)
+
+rm(taxaT)
+## ##################################################
+
+
+
+## ##################################################
+## From earlier experiments, we figured out these parameters work best
+## for the random forest model.
+
+## Number of bootstrap samples.
+numBtSamps <- 3000
+
+## Repeated cross-validation runs, leaving out 20% of the observations
+## at a time, indicated that the number of variables to consider at
+## each split is about 13 for the response variable in the original
+## units, closely followed by 12.
+numVarSplit <- 13
+## ##################################################
+
+
+
+## ##################################################
+## Run the cross-validation for this model, so that we can see what
+## the CV MSE looks like.
+
+set.seed(3313411)
+
+## Number of times to do cross-validation.
+numCVs <- 1000
+## How many observations to reserve for testing each time.
+numLeaveOut <- round(0.20 * nrow(wideT))
+
+
+## ###########################
+## Set up function for fitting random forest model using original
+## units.
+origUnitsF <- function(x, mtry, ntree){
+  rf <- randomForest(degdays ~ . , data=x$trainT, mtry=mtry, ntree=ntree, importance=T)
+  return(predict(rf, newdata=x$validT))
+}
+## ###########################
+
+
+## ###########################
+## Get set up for cross-validation.
+crossvalidL <- vector("list", numCVs)
+for (i in 1:numCVs){
+  lvOut <- sample(1:nrow(wideT), size=numLeaveOut, replace=F)
+  trainT <- wideT[-lvOut,]
+  validT <- wideT[lvOut,]
+  crossvalidL[[i]] <- list(trainT=trainT, validT=validT)
+}
+rm(i, lvOut, trainT, validT)
+
+## Conduct cross-validation.
+origFitL <- mclapply(crossvalidL, mc.cores=3, origUnitsF, mtry=numVarSplit, ntree=numBtSamps)
+## ###########################
+
+
+## ###########################
+## For matrix to hold cross-validation results.
+cvMSE <- rep(NA, numCVs)
+cvErrFrac <- rep(NA, numCVs)
+
+set.seed(5483089)
+
+
+## Do cross-validation.
+for (i in 1:numCVs){
+
+  ## Get the validation set for this run from the list.
+  validT <- crossvalidL[[i]][["validT"]]
+  
+  ## Calculate SSTotal for the cross-validation set.
+  SSTot <- sum( (validT$degdays-mean(validT$degdays))^2 )
+
+  ## Calculate the MSE and error fraction of the SS Total for the
+  ## validation data in the original units.
+  resid <- origFitL[[i]] - validT$degdays
+  cvMSE[i] <- mean(resid^2)
+  cvErrFrac[i] <- sum(resid^2)/SSTot
+  rm(resid)
+}
+rm(i, validT, SSTot)
+
+write_csv(data.frame(cvMSE, cvErrFrac), path="final_rf_orig_units_cvstats_all_data.csv")
+rm(cvMSE, cvErrFrac)
+## ##################################################
+
+
+
+## ##################################################
+## Fit the final random forest with all the data (no cross-validation).
+
+set.seed(222933)
+
+## Fit the random forest model on all the data (no cross-validation).
+rf <- randomForest(degdays ~ . , data=wideT, mtry=numVarSplit,
+                   ntree=numBtSamps, importance=T)
+
+init.fig.dimen(file=paste0("orig_units_all_data_orders_imp_plot.pdf"), width=8, height=6)
+varImpPlot(rf, main="Importance of eukaryote order taxa (orig. units, all time steps)")
+dev.off()
+
+
+## Find residuals:
+resids <- rf$predicted - wideT$degdays
+## Print out RMSE:
+sqrt( mean( resids^2 ) )
+## Estimate of explained variance, which R documentation calls "pseudo
+## R-squared"
+1 - ( sum(resids^2)/sum( (wideT$degdays - mean(wideT$degdays))^2 ) )
+## ##################################################
---
+++ eukaryote_data/only_orders/all_time_steps/hit_1perc_twice/organize_orders_hit_cutoff_twice_all_time_steps.r
@@ -0,0 +1,73 @@
+library("tidyverse")
+
+
+## When I organized the data, any taxa which didn't get to at least at
+## certain frequence (I made it 1%) for at least one day and one
+## individual during *the whole time period* got folded into the
+## "Rare" taxa group.  Now, I'm making the cutoff at 5%, so I need to
+## check that all of these taxa have at least one day during on which
+## the frequency exceeds the NEW cutoff for at least one individual.
+## Otherwise, those taxa should be considered "rare".
+freqCutoff <- 0.01
+
+
+
+## ##################################################
+## Are we dealing with phlya, orders, or families?
+taxalevel <- "orders"
+
+## Read in cleaned-up phyla, orders, or families taxa.
+taxaT <- read_csv(paste0("../../../", taxalevel, "_massaged.csv"))
+## ##################################################
+
+
+
+## ##################################################
+## Any taxa which don't make up at least a certain percentage (given
+## by freqCutoff) on at least 1 day for at least 2 cadavers will get
+## absorbed into the "rare" category.
+
+## Get list of taxa for which the percentage exceeds the
+## frequency cutoff for at least 2 individuals.
+freqTaxaT <- taxaT %>%
+  group_by(taxon, subj) %>%
+  summarize(maxFracBySubjDay = max(fracBySubjDay)) %>%
+  group_by(taxon) %>%
+  summarize(ctSubjExceed = sum(maxFracBySubjDay > freqCutoff)) %>%
+  filter(ctSubjExceed > 1) %>%
+  select(taxon)
+
+
+## Incorporate the taxa that occur less than the frequency cutoff into
+## the "rare" group.
+commontaxaT <- taxaT
+commontaxaT[!(commontaxaT$taxon %in% freqTaxaT$taxon), "taxon"] <- "Rare"
+commontaxaT <- commontaxaT %>%
+  group_by(days, degdays, subj, taxon) %>%
+  summarize(counts=sum(counts), fracBySubjDay=sum(fracBySubjDay)) %>%
+  ungroup()
+
+
+## Check that the fractions add up to 1, appropriately.
+unique(
+    commontaxaT %>%
+    group_by(days, subj) %>%
+    summarize(sumFracBySubjDay = sum(fracBySubjDay)) %>%
+    ungroup() %>%
+    select(sumFracBySubjDay)
+)
+
+
+## Remove the list of taxa names that satisfied the frequence cutoff.
+rm(freqTaxaT, taxaT)
+## ##################################################
+
+
+
+## ##################################################
+## Write out the data to use in analyses when considering using only
+## taxa with at least 5% on at least one subject and day during the
+## first 2 weeks.
+
+write.csv(commontaxaT, file="orders_hit_cutoff_twice_all_time_steps.csv", row.names=FALSE)
+## ##################################################
