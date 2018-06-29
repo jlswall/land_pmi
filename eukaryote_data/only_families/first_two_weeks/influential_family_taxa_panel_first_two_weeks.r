17c044354e3aec21fed67a20f79303792185cf6d
Jenise Swall <jenise.swall@gmail.com>
2018-06-29 17:12:08 -0400

    Made scatterplot for eukaryote family-level data, just for the first 15 days.

---
+++ eukaryote_data/only_families/first_two_weeks/influential_family_taxa_panel_first_two_weeks.r
@@ -0,0 +1,21 @@
+library("tidyverse")
+
+## These are the top 6 families (in terms of IncNodePurity) from the
+## model with the stricter inclusion criterion (hit_1perc_twice):
+topChoices <- c("Dipodascaceae", "Phaffomycetaceae", "Trichostomatia", "Debaryomycetaceae", "Rhabditida", "Pichiaceae")
+
+
+## Using the dataset without the weird observations from subj. A3, day 40.
+indivT <- read_csv("../../families_massaged.csv")
+
+chooseT <- indivT %>%
+  filter(taxon %in% topChoices) %>%
+  filter(days <= 15)
+chooseT$taxon <- factor(chooseT$taxon, levels=topChoices)
+
+ggplot(chooseT, aes(degdays, fracBySubjDay)) +
+  geom_point(aes(color=subj)) +
+  labs(x="Degree days", y="Fraction") +
+  facet_wrap(~taxon, scales="free_y")  ## Allow diff. y-scales across panels.
+  ## facet_wrap(~taxon)  ## Keep y-scales same across panels.
+ggsave("influential_family_taxa_panel_first_two_weeks.pdf", width=7, height=4, units="in")
