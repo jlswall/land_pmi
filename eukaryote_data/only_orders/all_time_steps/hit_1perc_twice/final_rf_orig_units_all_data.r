dbbc758686d6a0cd7cc8dd6d3d68e2adaa1292a0
Jenise Swall <jenise.swall@gmail.com>
2018-06-29 16:54:42 -0400

    Worked on the eukaryote, order-level, all time steps data, especially plots.

--- eukaryote_data/only_orders/all_time_steps/hit_1perc_twice/final_rf_orig_units_all_data.r
+++ eukaryote_data/only_orders/all_time_steps/hit_1perc_twice/final_rf_orig_units_all_data.r
@@ -83,7 +83,7 @@ for (i in 1:numCVs){
 rm(i, lvOut, trainT, validT)
 
 ## Conduct cross-validation.
-origFitL <- mclapply(crossvalidL, mc.cores=3, origUnitsF, mtry=numVarSplit, ntree=numBtSamps)
+origFitL <- mclapply(crossvalidL, mc.cores=4, origUnitsF, mtry=numVarSplit, ntree=numBtSamps)
 ## ###########################
 
 
@@ -95,7 +95,8 @@ cvErrFrac <- rep(NA, numCVs)
 set.seed(5483089)
 
 
-## Do cross-validation.
+## Now, calculate the various summary statistics for each cross-validation.
+residDF <- NULL
 for (i in 1:numCVs){
 
   ## Get the validation set for this run from the list.
@@ -104,15 +105,26 @@ for (i in 1:numCVs){
   ## Calculate SSTotal for the cross-validation set.
   SSTot <- sum( (validT$degdays-mean(validT$degdays))^2 )
 
+  ## Calculate the residuals for this validation set.
+  resid <- validT$degdays - origFitL[[i]]
+
+  ## Build a data frame with the actual response and the estimated
+  ## response.
+  iCaseDF <- data.frame(yactual=validT$degdays, yhat=origFitL[[i]],
+                        resid=resid)
+  ## Add this data frame to what we've already collected.
+  residDF <- rbind(residDF, iCaseDF)
+
+  
   ## Calculate the MSE and error fraction of the SS Total for the
   ## validation data in the original units.
-  resid <- origFitL[[i]] - validT$degdays
   cvMSE[i] <- mean(resid^2)
   cvErrFrac[i] <- sum(resid^2)/SSTot
-  rm(resid)
+  rm(resid, iCaseDF)
 }
 rm(i, validT, SSTot)
 
+write_csv(residDF, path="final_rf_orig_units_residuals_all_data.csv")
 write_csv(data.frame(cvMSE, cvErrFrac), path="final_rf_orig_units_cvstats_all_data.csv")
 rm(cvMSE, cvErrFrac)
 ## ##################################################
@@ -135,9 +147,45 @@ dev.off()
 
 ## Find residuals:
 resids <- rf$predicted - wideT$degdays
+
 ## Print out RMSE:
 sqrt( mean( resids^2 ) )
+## RMSE: 193.1597
+
 ## Estimate of explained variance, which R documentation calls "pseudo
 ## R-squared"
 1 - ( sum(resids^2)/sum( (wideT$degdays - mean(wideT$degdays))^2 ) )
+## Expl. frac.: 0.8766751
+## ##################################################
+
+
+
+## ##################################################
+## Make graph of just IncNodePurity alone.
+
+## Turn importance measures into a tibble, sorted by IncNodePurity in
+## increasing order.
+importanceT <- importance(rf) %>%
+  as.data.frame() %>% as_tibble() %>%
+  rownames_to_column("order") %>%
+  arrange(IncNodePurity)
+## Turn order names into factors, so that we can make the bar chart
+## with the bars in decreasing order.
+importanceT$order <- factor(importanceT$order, levels=importanceT$order)
+ggplot(importanceT %>% top_n(10, wt=IncNodePurity),
+       aes(x=order, y=IncNodePurity)) +
+  coord_flip() +
+  geom_col() +
+  labs(x="Order", y="Decrease in node impurity")
+ggsave(filename="orig_units_all_data_orders_barchart.pdf", height=2.5, width=4, units="in")
+## ##################################################
+
+## ##################################################
+## Make plot of residuals.
+
+ggplot(residDF, aes(x=yactual, y=resid)) +
+  geom_point() +
+  geom_hline(yintercept=0) + 
+  labs(x="Actual degree days", y="Error (actual - estimated)")
+ggsave(filename="orig_units_all_data_orders_residuals.pdf", height=3.5, width=4, units="in")
 ## ##################################################
