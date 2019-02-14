1b64f34d094df6edba13c0ee4706b8a251204c94
Jenise Swall <jswall@vcu.edu>
2019-02-14 15:05:57 -0500

    Moved the code for making publication plots to another file.

--- eukaryote_data/only_families/all_time_steps/hit_1perc_twice/make_publication_plots.r
+++ eukaryote_data/only_families/all_time_steps/hit_1perc_twice/make_publication_plots.r
@@ -49,81 +49,6 @@ numVarSplit <- 15
 
 
 
-## ##################################################
-## Fit the random forest using all the data (do not hold any back for
-## cross-validation).
-
-## ###########################
-## Set up function for fitting random forest model using full dataset.
-fitFullDataF <- function(x, mtry, ntree){
-
-  rf <- randomForest(degdays ~ . , data=x, mtry=mtry, ntree=ntree, importance=T)
-
-  ## Order the taxa according to decreasing values of the scaled
-  ## importance %IncMSE.  Return as a tibble, with %IncMSE column
-  ## renamed to PercIncMSE.
-  importanceT <- as.tibble(importance(rf), rownames="taxa") %>% rename(PercIncMSE=`%IncMSE`) %>% arrange(desc(PercIncMSE))
-
-  myresults <- list(predicted=rf[["predicted"]], y=rf[["y"]], importanceT=importanceT)
-
-  return(myresults)
-}
-## ###########################
-
-
-## ###########################
-## Set up list as input for the function, which is just the original
-## dataset repeated over and over.
-numRepeat <- 1000
-repDataL <- vector("list", numRepeat)
-for (i in 1:numRepeat)
-  repDataL[[i]] <- wideT
-## ###########################
-
-
-## ###########################
-## Now, fit random forests to the full dataset over and over.
-
-set.seed(718460)
-fullResultsL <- mclapply(repDataL, mc.cores=6, fitFullDataF, mtry=numVarSplit, ntree=numBtSamps)
-
-## Calculate the RMSE and pseudo-Rsquared for these runs with the full
-## dataset.
-fullRMSE <- rep(NA, 1000)
-fullRsq <- rep(NA, 1000)
-fullImportanceT <- NULL
-for (i in 1:length(fullResultsL)){
-
-  ## Get results from run i.
-  iTmp <- fullResultsL[[i]]
-
-  ## Find residuals:
-  resids <- iTmp$predicted - iTmp$y
-
-  ## Calculate the RMSE and pseudo-Rsquared:
-  fullRMSE[i] <- sqrt( mean( resids^2 ) )
-  fullRsq[i] <- 1.0 - ( sum(resids^2)/sum( (iTmp$y - mean(iTmp$y))^2 ) )
-
-  ## Store measures of importance in a long tibble.
-  fullImportanceT <- rbind(fullImportanceT, iTmp$importanceT)
-}
-rm(i, iTmp, resids)
-
-## See summary of %IncMSE (measure of importance) over all model runs.
-fullImportanceT %>% group_by(taxa) %>% summarize(meanPercIncMSE=mean(PercIncMSE), lbPercIncMSE=quantile(PercIncMSE, 0.025), ubPercIncMSE=quantile(PercIncMSE, 0.975)) %>% arrange(desc(meanPercIncMSE))
-
-## Get summary statistics for report.
-c(mean(fullRMSE), 1.96*sd(fullRMSE))
-## RMSE: 177.680949   2.411631
-c(mean(fullRsq), 1.96*sd(fullRsq))
-## Rsq: 0.895643282 0.002831568
-
-write_csv(data.frame(fullRMSE, fullRsq), path="cvstats_w_full_dataset_final_params.csv")
-rm(fullRMSE, fullRsq)
-## ###########################
-## ##################################################
-
-
 
 ## ##################################################
 ## Fit the final random forest with all the data (no cross-validation).
@@ -133,10 +58,6 @@ set.seed(2240635)
 ## Fit the random forest model on all the data (no cross-validation).
 rf <- randomForest(degdays ~ . , data=wideT, mtry=numVarSplit,
                    ntree=numBtSamps, importance=T)
-
-## init.fig.dimen(file=paste0("orig_units_all_data_families_imp_plot.pdf"), width=8, height=6)
-## varImpPlot(rf, main="Importance of eukaryotic family-level taxa (all time steps)")
-## dev.off()
 
 
 ## Find residuals:
@@ -151,96 +72,6 @@ sqrt( mean( resids^2 ) )
 1 - ( sum(resids^2)/sum( (wideT$degdays - mean(wideT$degdays))^2 ) )
 ## Expl. frac.: 0.8958074
 ## ##################################################
-
-
-
-## ## ##################################################
-## ## Make graph of just IncNodePurity alone.
-
-## ## Get the top "n" (whether 8, 10, whatever) influential taxa.
-## n <- 8
-
-## ## Turn importance measures into a tibble, sorted by IncNodePurity in
-## ## increasing order.
-## importanceT <- importance(rf) %>%
-##   as.data.frame() %>% as_tibble() %>%
-##   rownames_to_column("family") %>%
-##   arrange(IncNodePurity)
-## ## Turn family names into factors, so that we can make the bar chart
-## ## with the bars in decreasing order.
-## importanceT$family <- factor(importanceT$family, levels=importanceT$family)
-## ggplot(importanceT %>% top_n(n, wt=IncNodePurity),
-##        aes(x=family, y=IncNodePurity)) +
-##   coord_flip() +
-##   geom_col() +
-##   labs(x="Eukaryotic family-level taxa", y="Decrease in node impurity")
-## ggsave(filename="orig_units_all_data_families_IncNodePurity_barchart.pdf", height=2.5, width=4.5, units="in")
-## ## ##################################################
-
-
-
-## ##################################################
-## Make graph of just %IncMSE alone.
-
-## Get the top "n" (whether 8, 10, whatever) influential taxa.
-n <- 6
-
-## Turn importance measures into a tibble, sorted by IncNodePurity in
-## increasing order.
-importanceT <- importance(rf) %>%
-  as.data.frame() %>% as_tibble() %>%
-  rownames_to_column("family") %>%
-  arrange(`%IncMSE`)
-## Turn family names into factors, so that we can make the bar chart
-## with the bars in decreasing order.
-importanceT$family <- factor(importanceT$family, levels=importanceT$family)
-ggplot(importanceT %>% top_n(n, wt=`%IncMSE`),
-       aes(x=family, y=`%IncMSE`)) +
-  coord_flip() +
-  geom_col() +
-  labs(x="Eukaryotic family-level taxa", y="Mean % decrease in MSE when excluded")
-ggsave(filename="orig_units_all_data_families_PercIncMSE_barchart.pdf", height=2.5, width=4.5, units="in")
-## ##################################################
-
-
-
-## ##################################################
-## Make scatter plots of the percentages over time (by taxa) for the
-## top n taxa in terms of %IncMSE.
-
-## Get the top "n" (whether 8, 10, whatever) influential taxa.
-n <- 6
-
-## Save the names of the families that are in the top 10 in
-## terms of %IncMSE.
-topChoices <- as.character(importanceT %>% arrange(desc(`%IncMSE`)) %>% pull(family))[1:n]
-
-## Find the percentages for these taxa.
-chooseT <- taxaT %>%
-  filter(taxon %in% topChoices)
-chooseT$taxon <- factor(chooseT$taxon, levels=topChoices)
-
-ggplot(chooseT, aes(degdays, fracBySubjDay)) +
-  geom_point(aes(color=subj)) +
-  labs(x="Degree days", y="Fraction", color="Cadaver") +
-  theme(legend.title=element_text(size=rel(0.8)), legend.text=element_text(size=rel(0.8))) + 
-  ## Allow diff. y-scales across panels.
-  facet_wrap(~taxon, ncol=3, scales="free_y") 
-  ## facet_wrap(~taxa)  ## Keep y-scales same across panels.
-ggsave("infl_euk_family_all_data_scatter.pdf", width=8, height=4, units="in")
-## ##################################################
-
-
-
-## ## ##################################################
-## ## Make plot of residuals.
-
-## ggplot(residDF, aes(x=yactual, y=resid)) +
-##   geom_point() +
-##   geom_hline(yintercept=0) + 
-##   labs(x="Actual accumulated degree days", y="Error (actual - estimated)")
-## ggsave(filename="orig_units_all_data_families_residuals.pdf", height=3.5, width=3.5, units="in")
-## ## ##################################################
 
 
 
@@ -269,7 +100,7 @@ tlPanel <- ggplot(importanceT %>% top_n(n, wt=`%IncMSE`),
   theme_minimal() +
   coord_flip() +
   geom_col() +
-  labs(x="Eukaryotic Family-level Taxa", y="Mean % Decrease in MSE When Excluded from Random Forest Model")
+  labs(x="Eukaryotic Family-level Taxa", y="Mean % Decrease in MSE When Taxa Excluded")
 ## ########################
 
 
@@ -360,7 +191,7 @@ maxAxisLmt <- max(c(predvactT$logactual, predvactT$logpredicted), na.rm=T)
 Rsq <- with(predvactT, round(cor(actual, predicted)^2, 2))
 ## RMSE around 1:1 line, not regression line.
 RMSE <- round(sqrt(mean(resids^2)), 2)  
-blPanel <- ggplot(predvactT, aes(x=logactual, y=logpredicted)) +
+brPanel <- ggplot(predvactT, aes(x=logactual, y=logpredicted)) +
   geom_point() +
   geom_abline(slope=1, intercept=0) +
   ## annotate("text", x=50, y=1700, hjust=0, label=paste("R^2  ==", Rsq), parse=T) +
@@ -374,8 +205,9 @@ blPanel <- ggplot(predvactT, aes(x=logactual, y=logpredicted)) +
 
 ## ########################
 library("cowplot")
-plot_grid(tlPanel, trPanel, blPanel, labels=c("a", "b", "c", "d"), rel_widths=c(1.125, 1), rel_heights=c(1, 1))
-ggsave(file="relative_abundance_Rsq_rmse.pdf", width=8.5, height=4, units="in")
+## Try theme(plot.margin) when creating graphs to add some space.
+plot_grid(tlPanel, trPanel, blPanel, brPanel, labels=c("a", "b", "c", "d"))##, rel_widths=c(1.125, 1), rel_heights=c(1, 1))
+ggsave(file="relative_abundance_Rsq_rmse.pdf", width=8.5, height=8.5, units="in")
 ## ########################
 ## ##################################################
 
