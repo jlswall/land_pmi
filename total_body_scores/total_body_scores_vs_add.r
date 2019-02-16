4af425044c028c1f98d55659804b2ad31612c845
Jenise Swall <jenise.swall@gmail.com>
2019-02-16 17:35:56 -0500

    Finished code to make 2-panel plot with TBS vs. log10(ADD) and vs. ADD.

--- total_body_scores/total_body_scores_vs_add.r
+++ total_body_scores/total_body_scores_vs_add.r
@@ -1,7 +1,7 @@
 library("tidyverse")
 library("ggplot2")
 library("readxl")
-
+library("cowplot")
 
 ## ##################################################
 ## Read in data from Excel.
@@ -17,7 +17,7 @@ rawAllT <- read_excel(path=fileNm, range="A22:S27", col_names=F)
 ## which cadavers were placed).
 wideT <- rawAllT[,-c(2, 3)]
 colnames(wideT) <- c("subj", paste("ADD", listADDs, sep="_"))
-rm(fileNm)
+rm(fileNm, listADDs)
 
 ## Now, we need to go from wide to long format.
 intermedT <- gather(wideT, ADD_degdays, tbs, -subj)
@@ -26,8 +26,75 @@ intermedT <- gather(wideT, ADD_degdays, tbs, -subj)
 tbsADDT <- separate(intermedT, ADD_degdays, c("add", "degdays"), sep="_", convert=T)
 ## Remove the column that contains only the old "ADD" prefix.
 tbsADDT <- tbsADDT %>% select(-add)
+rm(intermedT)
+## ##################################################
 
-with(tbsADDT, plot(log(degdays), tbs))
+
+
+## ##################################################
+## Make scatterplot of TBS vs. log10(ADD), with regression line,
+## regression equation, and R squared superimposed.
+
+## For help with setting up the expression for the regression equation, see
+## https://stackoverflow.com/questions/7549694/adding-regression-line-equation-and-r2-on-graph
+linLM <- lm(tbs ~ log10(degdays), data=tbsADDT)
+l <- list(a = round(as.vector(coef(linLM))[1], 4),
+          b = round(as.vector(coef(linLM))[2], 4))
+eq <- as.character(as.expression(substitute(paste(hat(y) == a + b, "x"), l)))
+Rsq <- round(summary(linLM)$r.squared, 4)
+lPanel <- ggplot(tbsADDT, aes(x=log10(degdays), y=tbs)) +
+  geom_point() +
+  stat_smooth(method="lm", se=FALSE) +
+  annotate("text", x=2.2, y=7.0, hjust=0, label=eq, parse=TRUE, size=3) +
+  annotate("text", x=2.2, y=5.5, hjust=0,
+           label=paste("R^2  ==", Rsq), parse=T, size=3) +
+  theme_bw() +
+  theme(plot.margin=unit(rep(0.15, 4), "in")) +
+  labs(x="Log 10 Accumulated Degree Days", y="Total Body Score")
+
+rm(linLM, l, eq, Rsq)
+## ##################################################
+
+
+
+## ##################################################
+## Make scatterplot of TBS vs. ADD, with regression line,
+## regression equation, and R squared superimposed.
+
+## For help with setting up the expression for the regression equation, see
+## https://stackoverflow.com/questions/7549694/adding-regression-line-equation-and-r2-on-graph
+curveLM <- lm(tbs ~ degdays + I(degdays^2) + I(degdays^3), data=tbsADDT)
+## l <- list(a = round(as.vector(coef(curveLM))[1], 4),
+##           b1 = round(as.vector(coef(curveLM))[2], 4),
+##           b2 = round(as.vector(coef(curveLM))[3], 4),
+##           b3 = round(as.vector(coef(curveLM))[4], 4))
+paramLst <- as.list(as.numeric(format(c(
+    as.vector(coef(curveLM))[1],
+    as.vector(coef(curveLM))[2],
+    as.vector(coef(curveLM))[3],
+    as.vector(coef(curveLM))[4]), digits=4)))
+names(paramLst) <- c("a", "b1", "b2", "b3")
+eq <- as.character(as.expression(substitute(paste(hat(y) == a + b1, "x", b2, x^2 + b3, x^3), paramLst)))
+## eq <- as.character(as.expression(substitute(paste(hat(y) == a + b1, "x", b2, x^2 +, b3, x^3), paramLst)))
+Rsq <- round(summary(curveLM)$r.squared, 4)
+rPanel <- ggplot(tbsADDT, aes(x=degdays, y=tbs)) +
+  geom_point() +
+  stat_smooth(method="lm", formula="y ~ x + I(x^2) + I(x^3)", se=FALSE) +
+  annotate("text", x=168, y=7, hjust=0, label=eq, parse=TRUE, size=3) +
+  annotate("text", x=168, y=5.5, hjust=0,
+           label=paste("R^2  ==", Rsq), parse=T, size=3) +
+  theme_bw() +
+  theme(plot.margin=unit(rep(0.15, 4), "in")) +
+  labs(x="Accumulated Degree Days", y="Total Body Score")
+## ##################################################
+
+
+## ##################################################
+plot_grid(lPanel, rPanel, labels=c("a", "b"))
+ggsave(file="tbs_vs_add_2panels.pdf", height=4, width=8, units="in")
+## ##################################################
+
+
 
 ## ##################################################
 ## Code I was hoping to use to combine our ADDs with the total body
